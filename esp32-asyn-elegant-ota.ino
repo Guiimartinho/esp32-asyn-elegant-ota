7ac1c4fa161b6279f474ee3c5e9bbc8e6487a2da
Luiz Guilherme Martinho Sampaio Ito <41489663+Guiimartinho@users.noreply.github.com>
2022-07-27 13:05:24 -0300

    Update esp32-asyn-elegant-ota.ino

--- esp32-asyn-elegant-ota.ino
+++ esp32-asyn-elegant-ota.ino
@@ -19,8 +19,8 @@
 #include <AsyncElegantOTA.h>
 
 // Replace with your network credentials
-const char* ssid = "Virus_13_2g";
-const char* password = "259731gui";
+const char* ssid = "SSID";
+const char* password = "PASS";
 
 bool ledState = 0;
 const int ledPin = 2;
